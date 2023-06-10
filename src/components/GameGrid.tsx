6d46c9a111b36376d5fd364b49dfd71ed24f188b
PieroMastro <pier.andres@gmail.com>
2023-06-10 04:59:28 +0000

    minor fixes on GameCard component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -22,7 +22,7 @@ const GameGrid = ({ gameQuery }: Props) => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
-        spacing={4}
+        spacing={5}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,6 +2,7 @@ import useGenres, { Genre } from "../hooks/useGenres";
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -23,31 +24,39 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   if (isLoading) return <Spinner />;
 
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImgURL(genre.image_background)}
-            />
-            <Stack>
-              <Button
-                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-                onClick={() => onSelectGenre(genre)}
-                fontSize="lg"
-                variant="link"
-                textAlign="justify"
-                whiteSpace="pre-line"
-              >
-                {genre.name}
-              </Button>
-            </Stack>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="3xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedImgURL(genre.image_background)}
+              />
+              <Stack>
+                <Button
+                  fontWeight={
+                    genre.id === selectedGenre?.id ? "bold" : "normal"
+                  }
+                  onClick={() => onSelectGenre(genre)}
+                  fontSize="lg"
+                  variant="link"
+                  textAlign="justify"
+                  whiteSpace="pre-line"
+                >
+                  {genre.name}
+                </Button>
+              </Stack>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
