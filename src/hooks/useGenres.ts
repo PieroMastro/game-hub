60f547a14d1438ee001d9bf981cc2f64e5ae0e88
PieroMastro <pier.andres@gmail.com>
2023-07-27 19:56:53 +0000

    Refactor - Create lookup hooks & implementing static data for genres and platforms

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
+import genres from "../data/genres";
 
 const apiClient = new APIClient<Genre>("/genres");
 
@@ -14,7 +15,7 @@ const useGenres = () =>
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: genres, // --- Replaced fetching with static data to minimize loading ---
   });
-// --- Pending: replace fetching with static data to minimize loading ---
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient, { FetchResponse } from "../services/api-client";
 import platforms from "../data/platforms";
+import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
@@ -15,7 +15,7 @@ const usePlatforms = () =>
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
-    // initialData: { count: platforms.length, results: platforms }, || Need to implement static data later.
+    initialData: platforms, //  --- Replaced fetching with static data to minimize loading ---
 
     // () =>
     //   apiClient
